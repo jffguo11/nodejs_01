68b54d3c7bb3c739bf48c410d68090f029a2068b
jffguo11 <jffguo11@gmail.com>
2016-08-08 16:32:04 +0000

    nodejs_01

---
+++ introNode/app.js
@@ -0,0 +1,19 @@
+// var cat = require("cat-me");
+// console.log(cat());
+
+// var knockknock = require("knock-knock-jokes");
+// console.log(knockknock());
+
+// for(var i = 0; i < 10; i++){
+//     console.log("node.js");
+// }
+
+function echo(str, num){
+    for (var j = 1; j <= num; j++){
+        console.log(str);
+    }
+}
+
+echo("this cloud9",5);
+
+
